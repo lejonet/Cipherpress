06c7b1f04f7709f0a865d850aa58c83ad3644536
Daniel Kuehn <daniel@kuehn.se>
2011-07-31 17:31:39 +0200

    Changed named of the Post-class to avoid confusion

--- posts_object.py
+++ posts_object.py
@@ -3,28 +3,33 @@
 from markdown import *
 from model import *
 
-class Post:
+class Post_Object:
 	"""Class to make the posts into objects"""
 	def __init__(self):
-		setup_all()
 		self._posts = Posts.query.all()
 		self._titles = []
 		self._bodies = []
 		for post in self._posts:
 			self._titles.append(post.title)
 			self._bodies.append(post.body)
-		print self._titles, type(self._titles)
-		print self._bodies, type(self._bodies)
-		print self._posts, type(self._posts)
+#		print self._titles, type(self._titles)
+#		print self._bodies, type(self._bodies)
+#		print self._posts, type(self._posts)
 			
 	def markdown_titles(self):
 		return self._titles
 		
 	def html_titles(self):
-		return markdown(title)
+		self._markdown_titles = []
+		for title in self._titles:
+			self._markdown_titles.append(markdown(title))
+		return self._markdown_titles
 
 	def markdown_bodies(self):
 		return self._bodies
 
 	def html_bodies(self):
-		return markdown(body)
+		self._markdown_bodies = []
+		for body in self._bodies:
+			self._markdown_bodies.append(markdown(body))
+		return self._markdown_bodies
