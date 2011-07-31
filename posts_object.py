308138787883bfda1d947fdbd0908ba47c8464f7
Daniel Kuehn <daniel@kuehn.se>
2011-07-31 18:18:20 +0200

    Changed names on the methods to avoid confusing and better reflect what the method actually does

--- posts_object.py
+++ posts_object.py
@@ -6,30 +6,33 @@ from model import *
 class Post_Object:
 	"""Class to make the posts into objects"""
 	def __init__(self):
-		self._posts = Posts.query.all()
+#		self._db_posts = Posts()
+#		self._posts = self._db_posts.query.all()
 		self._titles = []
 		self._bodies = []
-		for post in self._posts:
+		setup_all()
+		self._db_posts = DB_Posts.query.all()
+		for post in self._db_posts:
 			self._titles.append(post.title)
 			self._bodies.append(post.body)
 #		print self._titles, type(self._titles)
 #		print self._bodies, type(self._bodies)
 #		print self._posts, type(self._posts)
 			
-	def markdown_titles(self):
+	def raw_titles(self):
 		return self._titles
 		
 	def html_titles(self):
-		self._markdown_titles = []
+		self._html_titles = []
 		for title in self._titles:
-			self._markdown_titles.append(markdown(title))
-		return self._markdown_titles
+			self._html_titles.append(markdown(title))
+		return self._html_titles
 
-	def markdown_bodies(self):
+	def raw_bodies(self):
 		return self._bodies
 
 	def html_bodies(self):
-		self._markdown_bodies = []
+		self._html_bodies = []
 		for body in self._bodies:
-			self._markdown_bodies.append(markdown(body))
-		return self._markdown_bodies
+			self._html_bodies.append(markdown(body))
+		return self._html_bodies
