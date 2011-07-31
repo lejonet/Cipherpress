ab7624629f08b8e1b0d42ff88a5a7a93593ca3e3
Daniel Kuehn <daniel@kuehn.se>
2011-07-31 13:13:15 +0200

    Some tweaks to the Post class to add Unicode handling

--- model.py
+++ model.py
@@ -2,12 +2,13 @@
 #-*- coding:utf-8 -*-
 from elixir import *
 
-metadata.bind = "sqlite://posts.sdb"
+metadata.bind = "sqlite:///posts.sdb"
 metadata.bind.echo = True
 
 class Post(Entity):
     """Model for the post table in the database"""
-    post = Field(Text, required=True)
-
+    title = Field(Unicode(64), required=True)
+    body = Field(UnicodeText, required=True)
+    
     def __repr__(self):
-        return '<Post "%s">' % (self.post)
+        return '<Post title: "%s" post: "%s">' % (self.title,self.post)
