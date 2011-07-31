b14a6575f5faada174592b320b733efd8ac8892f
Daniel Kuehn <daniel@kuehn.se>
2011-07-31 15:55:32 +0200

    Added some more debugging to model to debug unicode issues

--- model.py
+++ model.py
@@ -5,10 +5,10 @@ from elixir import *
 metadata.bind = "sqlite:///posts.sdb"
 metadata.bind.echo = True
 
-class Post(Entity):
+class Posts(Entity):
     """Model for the post table in the database"""
     title = Field(Unicode(64), required=True)
     body = Field(UnicodeText, required=True)
     
     def __repr__(self):
-        return '<Post title: "%s" post: "%s">' % (self.title,self.post)
+        return '<Post title: "%s" post: "%s" types: %s %s>' % (self.title,self.body,type(self.title),type(self.body))
