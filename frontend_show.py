63f5be514c3902e18ccd6a943f913febecc85bac
Daniel Kuehn <daniel@kuehn.se>
2011-08-02 22:22:12 +0200

    Changing name on the frontend part, seeing as it is going to be 2 frontends. One for showing, another for submitting

---
+++ frontend_show.py
@@ -0,0 +1,26 @@
+ #!/usr/bin/env python
+#-*- coding:utf-8 -*-
+import web
+from posts_object import *
+
+urls = ('/', 'index',
+	'/post/(.*)', 'posts')
+render = web.template.render('templates/')
+web_frontend = web.application(urls, globals())
+
+if __name__ == "__main__":
+	web_frontend.run()
+
+class index:
+	"""The function that routes what goes where"""
+	def GET(self):
+		posts_obj = Post_Object()
+		return render.index(posts_obj.raw_list())
+
+class posts:
+	"""The function to display posts"""
+
+	def GET(self, pid):
+		post_obj = Post_Object(pid)
+		return render.post(post_obj.html_list())
+		
